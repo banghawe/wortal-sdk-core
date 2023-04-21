6f74e017fa692eb443a26a485e0afe36452c73f7
Tim Dillon <tim@itsdits.com>
2023-04-21 16:20:47 +0900

    feat(player): add missing player apis (#120)
    * Added missing examples in docs
    * Added missing return values in docs

--- src/api/player.ts
+++ src/api/player.ts
@@ -1,10 +1,13 @@
 import ConnectedPlayer from "../models/connected-player";
 import { ConnectedPlayerPayload, PlayerData } from "../types/player";
+import { SignedASID } from "../types/signed-asid";
 import { invalidParams, notSupported, rethrowRakuten } from "../utils/error-handler";
 import { config } from "./index";
 
 /**
  * Gets the player's ID from the platform.
+ * @example
+ * Wortal.player.getID(); // 'Player123ABC'
  * @returns The player's ID.
  */
 export function getID(): string {
@@ -13,6 +16,8 @@ export function getID(): string {
 
 /**
  * Gets the player's name on the platform.
+ * @example
+ * Wortal.player.getName(); // 'Ragnar Lothbrok'
  * @returns The player's name.
  */
 export function getName(): string {
@@ -21,6 +26,8 @@ export function getName(): string {
 
 /**
  * Gets the player's photo from the platform.
+ * @example
+ * Wortal.player.getPhoto(); // 'data:image/png;base64,iVBORw0KGgoAAAANSUh..' (base64 encoded image)
  * @returns URL of base64 image for the player's photo.
  */
 export function getPhoto(): string {
@@ -29,7 +36,12 @@ export function getPhoto(): string {
 
 /**
  * Checks whether this is the first time the player has played this game.
- * @returns True if it is the first play. Some platforms always return true.
+ * @example
+ * if (Wortal.player.isFirstPlay()) {
+ *    // Show tutorial
+ *    Wortal.player.setDataAsync({ tutorialShown: true });
+ * }
+ * @returns True if it is the first play. Some platforms do not have data persistence and always return true.
  */
 export function isFirstPlay(): boolean {
     return config.player.isFirstPlay;
@@ -44,6 +56,7 @@ export function isFirstPlay(): boolean {
  *      console.log(data['lives']);
  *  });
  * @param keys Array of keys for the data to get.
+ * @returns Key-value pairs of the data.
  * @throws {ErrorMessage} See error.message for details.
  * <ul>
  * <li>INVALID_PARAM</li>
@@ -87,6 +100,7 @@ export function getDataAsync(keys: string[]): Promise<any> {
  *     lives: 3,
  * });
  * @param data Key-value pairs of the data to upload. Nullable values will remove the data.
+ * @returns Promise that resolves when the data is uploaded.
  * @throws {ErrorMessage} See error.message for details.
  * <ul>
  * <li>NOT_SUPPORTED</li>
@@ -107,6 +121,39 @@ export function setDataAsync(data: Record<string, unknown>): Promise<void> {
                 });
         } else {
             throw notSupported("Player API not currently supported on platform: " + platform, "player.setDataAsync");
+        }
+    });
+}
+
+/**
+ * Flushes any unsaved data to the platform's storage. This function is expensive, and should primarily be used for
+ * critical changes where persistence needs to be immediate and known by the game. Non-critical changes should rely on
+ * the platform to persist them in the background.
+ * NOTE: Calls to player.setDataAsync will be rejected while this function's result is pending.
+ * @example
+ * Wortal.player.flushDataAsync()
+ *  .then(() => console.log("Data flushed."));
+ * @returns Promise that resolves when the data is flushed.
+ * @throws {ErrorMessage} See error.message for details.
+ * <ul>
+ * <li>NOT_SUPPORTED</li>
+ * <li>RETHROW_FROM_PLATFORM</li>
+ * </ul>
+ */
+export function flushDataAsync(): Promise<void> {
+    let platform = config.session.platform;
+    return Promise.resolve().then(() => {
+        if (platform === "link" || platform === "viber" || platform === "facebook") {
+            return (window as any).wortalGame.player.flushDataAsync()
+                .catch((e: any) => {
+                    if (platform === "link" || platform === "viber") {
+                        throw rethrowRakuten(e, "player.flushDataAsync");
+                    } else {
+                        throw Error(e);
+                    }
+                });
+        } else {
+            throw notSupported("Player API not currently supported on platform: " + platform, "player.flushDataAsync");
         }
     });
 }
@@ -170,8 +217,8 @@ export function getConnectedPlayersAsync(payload?: ConnectedPlayerPayload): Prom
  *          gameDataToValidate,
  *      )
  *  });
- *  @returns Object with player ID and signature.
- *  @see Signature
+ * @returns Object with player ID and signature.
+ * @see Signature
  * @throws {ErrorMessage} See error.message for details.
  * <ul>
  * <li>NOT_SUPPORTED</li>
@@ -203,28 +250,131 @@ export function getSignedPlayerInfoAsync(): Promise<object> {
 }
 
 /**
- * Flushes any unsaved data to the platform's storage. This function is expensive, and should primarily be used for
- * critical changes where persistence needs to be immediate and known by the game. Non-critical changes should rely on
- * the platform to persist them in the background.
- * NOTE: Calls to player.setDataAsync will be rejected while this function's result is pending.
+ * A unique identifier for the player. This is the standard Facebook Application-Scoped ID which is used for all Graph
+ * API calls. If your game shares an AppID with a native game this is the ID you will see in the native game too.
  * @example
- * Wortal.player.flushDataAsync()
- *  .then(() => console.log("Data flushed."));
+ * Wortal.player.getASIDAsync()
+ * .then(asid => console.log("Player ASID: " + asid));
+ * @returns A unique identifier for the player. String is nullable.
+ * @throws {ErrorMessage} See error.message for details.
+ * <ul>
+ * <li>NOT_SUPPORTED</li>
+ * <li>RETHROW_FROM_PLATFORM</li>
+ * </ul>
  */
-export function flushDataAsync(): Promise<void> {
+export function getASIDAsync(): Promise<string> {
     let platform = config.session.platform;
     return Promise.resolve().then(() => {
-        if (platform === "link" || platform === "viber" || platform === "facebook") {
-            return (window as any).wortalGame.player.flushDataAsync()
+        if (platform === "facebook") {
+            return (window as any).wortalGame.player.getASIDAsync()
                 .catch((e: any) => {
-                    if (platform === "link" || platform === "viber") {
-                        throw rethrowRakuten(e, "player.flushDataAsync");
-                    } else {
+                    if (platform === "facebook") {
                         throw Error(e);
                     }
                 });
         } else {
-            throw notSupported("Player API not currently supported on platform: " + platform, "player.flushDataAsync");
+            throw notSupported("Player API not currently supported on platform: " + platform, "player.getASIDAsync");
+        }
+    });
+}
+
+/**
+ * A unique identifier for the player. This is the standard Facebook Application-Scoped ID which is used for all Graph
+ * API calls. If your game shares an AppID with a native game this is the ID you will see in the native game too.
+ * @example
+ * Wortal.player.getSignedASIDAsync()
+ *  .then(info => {
+ *     myServer.validate(
+ *     info.asid,
+ *     info.signature,
+ *     );
+ *   });
+ * @returns Object with player ASID and signature.
+ * @see SignedASID
+ * @throws {ErrorMessage} See error.message for details.
+ * <ul>
+ * <li>NOT_SUPPORTED</li>
+ * <li>RETHROW_FROM_PLATFORM</li>
+ * </ul>
+ */
+export function getSignedASIDAsync(): Promise<SignedASID> {
+    let platform = config.session.platform;
+    return Promise.resolve().then(() => {
+        if (platform === "facebook") {
+            return (window as any).wortalGame.player.getSignedASIDAsync()
+                .then((info: any) => {
+                    return {
+                        asid: info.getASID(),
+                        signature: info.getSignature(),
+                    };
+                })
+                .catch((e: any) => {
+                    if (platform === "facebook") {
+                        throw Error(e);
+                    }
+                });
+        } else {
+            throw notSupported("Player API not currently supported on platform: " + platform, "player.getSignedASIDAsync");
+        }
+    });
+}
+
+/**
+ * Checks if the current user can subscribe to the game's bot.
+ * @example
+ * Wortal.player.canSubscribeBotAsync()
+ * .then(canSubscribe => console.log("Can subscribe to bot: " + canSubscribe));
+ * @returns Whether a player can subscribe to the game bot or not. Developer can only call subscribeBotAsync() after
+ * checking canSubscribeBotAsync(), and the game will only be able to show the player their bot subscription
+ * dialog once per week.
+ * @throws {ErrorMessage} See error.message for details.
+ * <ul>
+ * <li>NOT_SUPPORTED</li>
+ * <li>RETHROW_FROM_PLATFORM</li>
+ * </ul>
+ */
+export function canSubscribeBotAsync(): Promise<boolean> {
+    let platform = config.session.platform;
+    return Promise.resolve().then(() => {
+        if (platform === "facebook") {
+            return (window as any).wortalGame.player.canSubscribeBotAsync()
+                .catch((e: any) => {
+                    if (platform === "facebook") {
+                        throw Error(e);
+                    }
+                });
+        } else {
+            throw notSupported("Player API not currently supported on platform: " + platform, "player.canSubscribeBotAsync");
+        }
+    });
+}
+
+/**
+ * Request that the player subscribe the bot associated to the game. The API will reject if the subscription fails -
+ * else, the player will subscribe the game bot.
+ * @example
+ * Wortal.player.subscribeBotAsync()
+ * .then(() => console.log("Player subscribed to bot"));
+ * @returns Promise that resolves when the player has subscribed to the game bot. Rejects if the player is unable to
+ * subscribe to the game bot.
+ * @throws {ErrorMessage} See error.message for details.
+ * <ul>
+ * <li>NOT_SUPPORTED</li>
+ * <li>RETHROW_FROM_PLATFORM</li>
+ * </ul>
+ */
+export function subscribeBotAsync(): Promise<void> {
+    let platform = config.session.platform;
+    return Promise.resolve().then(() => {
+        if (platform === "facebook") {
+            return (window as any).wortalGame.player.subscribeBotAsync()
+                .catch((e: any) => {
+                    if (platform === "facebook") {
+                        throw Error(e);
+                    }
+                });
+        } else {
+            throw notSupported("Player API not currently supported on platform: " + platform, "player.subscribeBotAsync");
         }
     });
 }
