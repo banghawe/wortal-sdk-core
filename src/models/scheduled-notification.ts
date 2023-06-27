f661c7c9ee7d7b9ecaaa111127489d4490ed1a81
Tim Dillon <tim@itsdits.com>
2023-06-27 11:34:56 +0900

    docs: add docs for scheduled-notification

--- src/models/scheduled-notification.ts
+++ src/models/scheduled-notification.ts
@@ -1,26 +1,41 @@
 import { NotificationData } from "../types/notification-data";
 import { NotificationStatus } from "../types/notification-status";
 
-/** @hidden */
+/**
+ * Notification that has been scheduled.
+ */
 export default class ScheduledNotification {
     private notificationData: NotificationData;
 
+    /** @hidden */
     constructor(data: NotificationData) {
         this.notificationData = data;
     }
 
+    /**
+     * Unique ID of the notification.
+     */
     get id(): string {
         return this.notificationData.id;
     }
 
+    /**
+     * Current status of the notification.
+     */
     get status(): NotificationStatus {
         return this.notificationData.status;
     }
 
+    /**
+     * Optional label for the notification.
+     */
     get label(): string | undefined {
         return this.notificationData.label;
     }
 
+    /**
+     * Time the notification was created.
+     */
     get createdTime(): string {
         return this.notificationData.createdTime;
     }
