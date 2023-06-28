a57dabbfc03fbf1b001664d766bf4e155c3e51ca
Tim Dillon <tim@itsdits.com>
2023-06-28 11:50:29 +0900

    chore: update to wortal.js v1.4.1

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const Wortal = _Wortal;
 // We need to include the Wortal SDK backend interface. Nothing should run until this is loaded.
 let script = document.createElement("script");
 script.type = "text/javascript";
-script.src = "https://storage.googleapis.com/html5gameportal.com/embeds/wortal-1.4.0.js";
+script.src = "https://storage.googleapis.com/html5gameportal.com/embeds/wortal-1.4.1.js";
 script.onload = () => Wortal._initInternal();
 document.head.appendChild(script);
 
