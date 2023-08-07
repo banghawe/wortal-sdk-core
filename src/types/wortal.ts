611857715bd614b3846beaf73535931ff6f0c40a
Tim Dillon <tim@itsdits.com>
2023-08-07 09:35:26 +0900

    feat(core): add wortal types

---
+++ src/types/wortal.ts
@@ -0,0 +1,14 @@
+/** @hidden */
+export const PLATFORM_DOMAINS = {
+    "viber": ["vbrplsbx.io"],
+    "link": ["rgsbx.net", "lgsbx.net"],
+    "wortal": ["html5gameportal.com", "html5gameportal.dev"],
+    "gd": ["revision.gamedistribution.com", "gamedistribution.com", "html5.gamedistribution.com"],
+    "facebook": ["facebook.com", "www.facebook.com", "apps.fbsbx.com"],
+}
+
+/** @hidden */
+export type ShareTo = ["facebook", "twitter"]
+
+/** @hidden */
+export type APIEndpoints = "https://html5gameportal.com/api"
