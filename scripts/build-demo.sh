23faf9a4285c2c76dcec9031eedc7070d608bb70
Tim Dillon <tim@itsdits.com>
2023-08-17 12:35:13 +0900

    build: remove version number from output file

--- scripts/build-demo.sh
+++ scripts/build-demo.sh
@@ -11,7 +11,7 @@ if [ -f demo.zip ]; then
 fi
 
 # Find the latest build
-latest_sdk=$(find "$SDK_DIR"/wortal-core-*.js | head -n 1)
+latest_sdk=$(find "$SDK_DIR"/wortal-core.js | head -n 1)
 if [ -z "$latest_sdk" ]; then
   echo "No wortal-core.js file found in $SDK_DIR"
   exit 1
@@ -19,15 +19,5 @@ fi
 
 # Copy the latest build into the demo directory
 cp "$latest_sdk" .
-
-# Get a reference to the copied file
-moved_sdk=$(find wortal-core-*.js | head -n 1)
-if [ -z "$latest_sdk" ]; then
-  echo "No wortal-core.js file found in $DEMO_DIR"
-  exit 1
-fi
-
-# Trim the version number to make it easier to include in index.html
-mv "$moved_sdk" "wortal-core.js"
 
 zip -r demo.zip ./*
